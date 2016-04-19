ac37d8b680dc3160fc2460f0e46f5cde28ea3b3b
Kenneth Burgener <kenneth@oeey.com>
2016-04-19 14:46:50 -0600

    doc about authentication issue

--- ksmtp/__init__.py
+++ ksmtp/__init__.py
@@ -1,15 +1,25 @@
 """
-KSMTP - Simple Python SMTP relay replacement for sendmail with SSL authentication
 
+ksmtp - Simple Python SMTP relay replacement for sendmail with SSL authentication
+
+
+ksmtp
+=====
 
 Simple Python SMTP relay replacement for sendmail with SSL authentication
 
 Useful for relaying all email through an account like Gmail, without the
 messy configurations of Postfix / Sendmail.
 
-Similar to the 'ssmtp' project, but written in Python
+Source
+======
 
-Usage:
+PyPI - https://pypi.python.org/pypi/ksmtp
+GitHub - https://github.com/oeey/ksmtp
+
+Usage
+=====
+
   1) pip install ksmtp
   2) edit /etc/ksmtp.conf with your login credentials
   3) (optional) create symlink to ksmtp to replace sendmail
@@ -17,8 +27,13 @@ Usage:
   4) send test mail
      ksmtp test@test.com -s "some subject"
 
-PyPI - https://pypi.python.org/pypi/ksmtp
-GitHub - https://github.com/oeey/ksmtp
+Issues
+======
+
+Gmail:
+If you get an "smtplib.SMTPAuthenticationError" and your credentials are
+correct, you may need to "allow less secure apps access" to your account.
+
+See https://support.google.com/accounts/answer/6010255
 
 """
-
