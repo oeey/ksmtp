02aeaab135f834db51fde0037c226c2d589fa593
Kenneth Burgener <kenneth@oeey.com>
2016-04-19 15:56:39 -0600

    added python library version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ksmtp',
-    version='0.2.2',
+    version='0.2.3',
     author='Kenneth Burgener',
     author_email='kenneth@oeey.com',
     scripts=['bin/ksmtp'],
